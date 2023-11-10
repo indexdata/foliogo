8fe0e9f085b715b63f67ef2a7df9f19939adb0c7
Mike Taylor <mike@indexdata.com>
2023-11-10 14:34:08 +0000

    Sample program is closer to being correct

--- bin/folio-list-users.go
+++ bin/folio-list-users.go
@@ -1,15 +1,12 @@
 package main
 
-import "os"
 import "fmt"
 import "github.com/indexdata/foliogo"
 
 func main() {
-	service := Folio.service('https://folio-snapshot-okapi.dev.folio.org')
-	session := service.login('diku', 'user-basic-view', 'user-basic-view')
-        body := session.folioFetch('/users?limit=20')
+	service := foliogo.NewService("https://folio-snapshot-okapi.dev.folio.org")
+	session := service.login("diku", "user-basic-view", "user-basic-view")
+	body := session.folioFetch("/users?limit=20")
 	fmt.Println(body.users)
-        session.close()
+	session.close()
 }
-
-
