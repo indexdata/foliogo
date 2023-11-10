e61ed84cef35b290449284cc31df62dff36a4d56
Mike Taylor <mike@indexdata.com>
2023-11-10 14:40:33 +0000

    More explicit output from test script

--- bin/folio-list-users.go
+++ bin/folio-list-users.go
@@ -5,8 +5,11 @@ import "github.com/indexdata/foliogo"
 
 func main() {
 	service := foliogo.NewService("https://folio-snapshot-okapi.dev.folio.org")
+	fmt.Printf("got service %+v", service)
+	/*
 	session := service.login("diku", "user-basic-view", "user-basic-view")
 	body := session.folioFetch("/users?limit=20")
 	fmt.Println(body.users)
 	session.close()
+	*/
 }
