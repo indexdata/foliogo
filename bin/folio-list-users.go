573b1d6a89ec09716b482362f487ac9405d2fa82
Mike Taylor <mike@indexdata.com>
2023-11-14 15:41:26 +0000

    Handle login errors

--- bin/folio-list-users.go
+++ bin/folio-list-users.go
@@ -1,12 +1,17 @@
 package main
 
+import "os"
 import "fmt"
 import "github.com/indexdata/foliogo"
 
 func main() {
 	service := foliogo.NewService("https://folio-snapshot-okapi.dev.folio.org")
 	fmt.Printf("got service %+v\n", service)
-	session := service.Login("diku", "user-basic-view", "user-basic-view")
+	session, err := service.Login("diku", "user-basic-view", "user-basic-view")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: login failed: %s\n", os.Args[0], err)
+		os.Exit(1)
+	}
 	fmt.Printf("got session %+v\n", session)
 	/*
 	body := session.folioFetch("/users?limit=20")
