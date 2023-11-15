de0e879d74036d596c0617f99eb51f5e69072542
Mike Taylor <mike@indexdata.com>
2023-11-15 15:53:11 +0000

    Complete the sample program

--- bin/folio-list-users.go
+++ bin/folio-list-users.go
@@ -6,16 +6,27 @@ import "github.com/indexdata/foliogo"
 
 func main() {
 	service := foliogo.NewService("https://folio-snapshot-okapi.dev.folio.org")
-	fmt.Printf("got service %+v\n", service)
 	session, err := service.Login("diku", "user-basic-view", "user-basic-view")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s: login failed: %s\n", os.Args[0], err)
 		os.Exit(1)
 	}
-	fmt.Printf("got session %+v\n", session)
-	/*
-	body := session.folioFetch("/users?limit=20")
-	fmt.Println(body.users)
-	session.close()
-	*/
+	body, err := session.Fetch("users?limit=20", foliogo.RequestParams{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: fetch users failed: %s\n", os.Args[0], err)
+		os.Exit(2)
+	}
+
+	// Unfortunatly, the rendering part is clumsy in Go
+	users := body["users"].([]interface{})
+	for _, e := range users {
+		user := e.(map[string]interface{})
+		var marker string
+		if user["active"] == true {
+			marker = "*"
+		} else {
+			marker = " "
+		}
+		fmt.Println(marker, user["username"])
+	}
 }
