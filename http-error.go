73f57139b59d31e51942b1b28d000f737fd8b3bf
Mike Taylor <mike@indexdata.com>
2023-11-15 11:33:36 +0000

    New file

---
+++ http-error.go
@@ -0,0 +1,22 @@
+package foliogo
+
+import "fmt"
+import "net/http"
+
+type httpError struct {
+	status int
+	method string
+	url string
+}
+
+func MakeHttpError(status int, 	method string, url string) *httpError {
+	return &httpError{status: status, method: method, url: url}
+}
+
+func (this httpError) String() string {
+	return fmt.Sprintf("HTTP error %d (%s): %s %s", this.status, http.StatusText(this.status), this.method, this.url)
+}
+
+func (this httpError) Error() string {
+	return this.String()
+}
